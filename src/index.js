fa29a78bf4f3eb8c2dd2c162a7413faf3c80a2e6
dvrakry <dv.rakry@gmail.com>
2022-05-23 09:05:59 +0900

    2022-05-23

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import dotenv from 'dotenv';
-dotenv.config();
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
 const mongoose = require('mongoose');
 
 const api = require('./api');
 
-//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
@@ -22,16 +23,17 @@ mongoose
 const app = new Koa();
 const router = new Router();
 
-//라우터 설정
-router.use('/api', routes());
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
 
-//라우터 적용 전에 bodyParser 적용
+// 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
 
-//app 인스터스에 라우터 적용
+// app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
+// PORT가 지정되어 있지 않다면 4000을 사용
 const port = PORT || 4000;
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log('Listening to port %d', port);
 });
